b0e40f5e92c2b3c65d08b6ddbc5b714f02b83686
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2024-12-10 10:26:01 +0300

    В сервер статистики добавлены pom- и docker-файлы.

---
+++ stats/stats-client/src/main/java/ru/practicum/MainApplication.java
@@ -0,0 +1,10 @@
+package ru.practicum;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world from stats-client");
+    }
+}
