495de16198d697f2acaeb1185e29afba826a3091
iamdnikolaev <160739477+iamdnikolaev@users.noreply.github.com>
2024-12-15 15:47:57 +0300

    Добавлен вариант схемы БД основного сервиса.

--- main-service/src/main/java/ru/practicum/MainApplication.java
+++ main-service/src/main/java/ru/practicum/MainApplication.java
@@ -1,10 +1,11 @@
 package ru.practicum;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world from main-service");
+        SpringApplication.run(MainApplication.class, args);
     }
 }
