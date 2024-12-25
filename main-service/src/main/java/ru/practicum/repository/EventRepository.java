56e347f33fe8f21bd47d8f3e9501c9926452b604
nikita.stogov <stogovn@yandex.ru>
2024-12-25 19:39:18 +0400

    fix: добавил валидацию для прохождения тестов

--- main-service/src/main/java/ru/practicum/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/repository/EventRepository.java
@@ -25,12 +25,12 @@ public interface EventRepository extends JpaRepository<Event, Long> {
                    "WHERE e.state = 'PUBLISHED' " +
                    "AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) " +
                    "OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%'))) " +
-                   "AND (:categories IS NULL OR e.category_id IN :categories) " +
+                   "AND (:categories IS NULL OR e.category_id = ANY(COALESCE(:categories, '{}'))) " +
                    "AND (:paid IS NULL OR e.paid = :paid) " +
                    "AND (e.event_date >= :rangeStart) " +
                    "AND (e.event_date <= :rangeEnd)", nativeQuery = true)
     List<Event> findAllEvents(@Param("text") String text,
-                              @Param("categories") List<Long> categories,
+                              @Param("categories") Long[] categories,
                               @Param("paid") Boolean paid,
                               @Param("rangeStart") LocalDateTime rangeStart,
                               @Param("rangeEnd") LocalDateTime rangeEnd);
